a34513403cc48f7e1413e6b3794c63e2c02baf40
angeD <ange.duhayon@epitech.eu>
2022-02-12 14:34:37 +0100

    v0.1 [MOD] replaced required by modifier

--- contracts/CreateTickets.sol
+++ contracts/CreateTickets.sol
@@ -12,8 +12,12 @@ import "./SikkerStats.sol";
 contract CreateTickets is SikkerStats {
     using SafeMath for uint256;
 
-    function _CreateTicket(type_t _type, bool _specificity, uint8 _lossPercent, uint256 _amount, address _receiver, uint256 _timeLock, bytes32 _hash)
-        public payable {
+    modifier posAmount(uint256 _amount) {
+        require(_amount > 10000, "You must enter a non-null amount");
+        _;
+    }
+
+    function createTicket(type_t _type, bool _specificity, uint8 _lossPercent, uint256 _amount, address _receiver, uint256 _timeLock, bytes32 _hash) public payable {
 
         require(_amount > 10000, "You must require > 10000 wei");
 
@@ -21,16 +25,14 @@ contract CreateTickets is SikkerStats {
 
         ticket.Amount = _amount;
         ticket.Creator = msg.sender;
-
         ticket.Hash = _hash;
         ticket.Status = 0;
-    
         ticket.Specificity = _specificity;
         if (_type == type_t.CE)
             ticket.Receiver = payable(_receiver);
         else if (_specificity)
-                ticket.Receiver = payable(_receiver);
-        ticket.Timelock = _timeLock != 0 ? block.timestamp.add(_timeLock) : 0;
+            ticket.Receiver = payable(_receiver);
+        ticket.TimeLock = _timeLock != 0 ? block.timestamp.add(_timeLock) : 0;
         ticket.LossPercent = _lossPercent > 5 && _lossPercent <= 100 ? _lossPercent : 15;
         tickets.push(ticket);
     }
